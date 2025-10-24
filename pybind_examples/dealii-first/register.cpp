02afce2a920c49cc63413a3d74b79efc73a525a1
Matteo Poggi <matteo.poggi.fi@gmail.com>
2025-10-24 15:34:56 +0200

    Add dealii example

---
+++ pybind_examples/dealii-first/register.cpp
@@ -0,0 +1,14 @@
+#include <deal.II/dofs/dof_handler.h>
+#include <deal.II/grid/grid_generator.h>
+
+#include <pybind11/pybind11.h>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(dealII_coral, m, py::mod_gil_not_used())
+{
+    using namespace dealii;
+
+    py::class_<Triangulation<2>>(m, "Triangulation2")
+        .def(py::init<>());
+}
