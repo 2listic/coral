41249a7c1c883ca024f2ce6f18c8debc5c5fa495
Matteo Poggi <matteo.poggi.fi@gmail.com>
2025-11-15 16:00:06 +0100

    move bashrc injection to Dockerfile.

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,5 +1,5 @@
 #/bin/bash
 
-PYBIND11_CMAKE_DIR=$(python3 -m pybind11 --cmakedir) && \
-echo "export CMAKE_PREFIX_PATH=${PYBIND11_CMAKE_DIR}:${CMAKE_PREFIX_PATH}" >> ~/.bashrc
+# PYBIND11_CMAKE_DIR=$(python3 -m pybind11 --cmakedir) && \
+# echo "export CMAKE_PREFIX_PATH=${PYBIND11_CMAKE_DIR}:${CMAKE_PREFIX_PATH}" >> ~/.bashrc
 
